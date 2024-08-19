b156446d5d3476f45a74291b7f69c0869c7e13aa
Michelle Nesbitt <mn218@nerdymail.com>
2024-08-19 13:53:16 -0400

    Update Repo

--- DoggyEventsAPI/Program.cs
+++ DoggyEventsAPI/Program.cs
@@ -10,10 +10,12 @@ using Microsoft.OpenApi.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
